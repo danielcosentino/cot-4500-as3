4aab495d384354c659434d4080f0091c7bad91ee
Cosentino <daniel.cosentino@knights.ucf.edu>
2023-03-30 15:15:35 -0400

    questions 4,5,6

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 # question 1
 def euler_method(f, t_range, num_steps, y0):
@@ -47,8 +48,8 @@ print(y_final)
 print()
 
 # question 3
-def printMatrix(r1, r2, r3, width=6):
-  matrix = [r1, r2, r3]
+def printMatrix(matrix, width=6):
+  # matrix = [r1, r2, r3]
   for row in matrix:
     row_str = " ".join([str(x).center(width) for x in row])
     print(row_str)
@@ -94,3 +95,76 @@ for i in range(4):
 
 question_3 = np.array([int(r1[3]), int(r2[3]), int(r3[3])])
 print(question_3)
+print()
+
+# question 4
+def lu_factorization(matrix, epsilon=1e-8):
+  n = len(matrix)
+  L = [[0.0] * n for i in range(n)]
+  U = [[0.0] * n for i in range(n)]
+
+  det_parity = 1.0
+  for j in range(n):
+    L[j][j] = 1.0
+    for i in range(j+1):
+      s1 = sum(U[k][j] * L[i][k] for k in range(i))
+      U[i][j] = matrix[i][j] - s1
+    for i in range(j, n):
+      s2 = sum(U[k][j] * L[i][k] for k in range(j))
+      L[i][j] = (matrix[i][j] - s2) / U[j][j] if abs(U[j][j]) > epsilon else 0.0
+    if abs(U[j][j]) < epsilon:
+      U[j][j] += epsilon
+      det_parity *= -1.0
+
+  det = det_parity * reduce(lambda x, y: x * y, (U[i][i] for i in range(n)))
+  return det, np.array(L), np.array(U)
+
+m = [[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]]
+det, L, U = lu_factorization(m)
+print(det - 1e-14)
+print()
+print(L)
+print()
+print(U)
+print()
+
+# question 5
+def is_diagonally_dominant(matrix):
+  n = len(matrix)
+  for i in range(n):
+    diagonal_element = abs(matrix[i][i])
+    off_diagonal_sum = sum(abs(matrix[i][j]) for j in range(n) if j != i)
+    if diagonal_element <= off_diagonal_sum:
+      return False
+  return True
+
+ques5_matrix = [[9, 0, 5, 2, 1],
+                [3, 9, 1, 2, 1],
+                [0, 1, 7, 2, 3],
+                [4, 2, 3, 12, 2],
+                [3, 2, 4, 0, 8]]
+
+print(is_diagonally_dominant(ques5_matrix))
+print()
+
+# question 6
+def is_positive_definite(matrix):
+  n = len(matrix)
+  for i in range(n):
+    for j in range(i):
+      if matrix[i][j] != matrix[j][i]:
+        # Matrix is not symmetric
+        return False
+  eigenvalues = np.linalg.eigvals(matrix)
+  if any(eigenvalues <= 0):
+    # Matrix has non-positive eigenvalues
+    return False
+  # Matrix is positive definite
+  return True
+
+ques6_matrix = [[2, 2, 1],
+                [2, 3, 0],
+                [1, 0, 2]]
+
+print(is_positive_definite(ques6_matrix))
+print()
